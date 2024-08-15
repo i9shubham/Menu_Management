f2661c621bd9f0150087fd722a35cf94ed6b9553
Shubhahm Ingole <ishubham158@gmail.com>
2024-08-16 02:06:10 +0530

    add: centralized import and export of routes

---
+++ src/routes/index.js
@@ -0,0 +1,9 @@
+import express from 'express';
+
+import categoryRoutes from './categoryRoutes.js';
+
+const router = express.Router();
+
+router.use('/categories', categoryRoutes);
+
+export default router;
