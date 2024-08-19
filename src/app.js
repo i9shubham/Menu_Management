19e04cb92875aa4a8cef5dd7375f194e3ada07c9
Shubhahm Ingole <ishubham158@gmail.com>
2024-08-19 21:52:46 +0530

    add: cors for cross resource sharing

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import { connectDB } from './db/db.js';
 import routes from './routes/index.js';
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors()); //allowing all origins to access the server for cross resource sharing
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
